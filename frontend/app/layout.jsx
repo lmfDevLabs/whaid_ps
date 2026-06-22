238b68ea2a65757447f04959bcaa064b7ac6690a
LMF Dev Labs <145071450+lmfDevLabs@users.noreply.github.com>
2026-06-22 13:18:01 -0500

    Add Google Analytics tag

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -20,6 +20,18 @@ export default function RootLayout({ children }) {
     <html lang="es" data-theme="light">
       <body>
         {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YEFM85QX6V"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YEFM85QX6V');
+          `}
+        </Script>
         <Script src="/wa-chat.js" strategy="afterInteractive" />
         <Script src="/site.js" strategy="afterInteractive" />
       </body>
